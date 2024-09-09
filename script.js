423b24f26284ca07329b6fb946670701565e006d
Taric <taric.santos@escola.pr.gov.br>
2024-09-09 20:09:42 -0300

    dsn

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 // Adiciona uma classe ao cabeçalho quando o usuário rola a página
 window.addEventListener('scroll', function() {
     const header = document.getElementById('header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
-
-// Suaviza o scroll para âncoras internas
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('a[href^="#"]');
-    links.forEach(link => {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
-            
-            targetElement.scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-            });
-        });
-    });
-});
+    if (window.scrollY >
